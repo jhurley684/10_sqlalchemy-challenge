5f7b1f235ffc0dcfb80a3bd9d58aec1e157ed99d
Jim Hurley <jhurley684@gmail.com>
2021-11-08 15:27:09 -0600

    Added most active station and started search to previous year

--- Instructions/app.py
+++ Instructions/app.py
@@ -38,34 +38,19 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precip<br/>"
-        f"/api/v1.0/stations<br>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/active_station<br/>"
+        
     )
 
-# ############  STATIONS  #####################
-@app.route("/api/v1.0/stations")
-def station():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
 
-    """Return a list of stations  from data set"""
-    # Query all station measurements in Staton measurements
-    results = session.query(Station.station).all()
 
-    session.close()
-
-    # Convert list of tuples into normal list
-    all_station = list(np.ravel(results))
-
-    return jsonify(all_station)
-
-############  PRECIPITITATION  ################################
+###############   PRECIPITATION  ##########################
 @app.route("/api/v1.0/precip")
 def precip():
     # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
+   
     results = session.query(Measurement.date, Measurement.prcp).all()
 
     session.close()
@@ -82,37 +67,71 @@ def precip():
 
 
 
-##########  MOST ACTIVE STATIONS  ##########################
-@app.route("/api/v1.0/active_stations")
-def precip():
+#############  STATIONS #########################
+@app.route("/api/v1.0/stations")
+def station():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    # Query to find station that took most measurements (prcp)
-    results = session.query(Measurement.station, func.count(Measurement.prcp)).\
-        group_by(Measurement.station).\
-        order_by(func.count(Measurement.prcp).desc()).all()
+    """Return a list of stations  from data set"""
+    # Query all station measurements in Staton measurements
+    results = session.query(Station.station).all()
 
     session.close()
 
-    # Create a dictionary from the row data and append to a list of station_activity
-    station_activity = []
-    for date, prcp in results:
-        activity_dict = {}
-        activity_dict["station"] = station
-        activity_dict["measurements"] = prcp
-        station_activity.append(activity_dict)
+    # Convert list of tuples into normal list
+    all_station = list(np.ravel(results))
 
-    return jsonify(station_activity)
+    return jsonify(all_station)
 
 
+# ##########  TOBS BY MOST ACTIVE STATION  ##########################
+
+@app.route("/api/v1.0/active_station")
+def active_station():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query for station activity.  Take the most active station.
+    results = session.query(Measurement.station, func.count(Measurement.prcp)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.prcp).desc()).first()
+
+# # Query for all tobs measurements at each station. Filter for only the most active station (above)
+    session.query(Measurement.station, Measurement.data, Measurement.tobs ).\
+        filter( Measurement.station == results).\
+        filter(year_ago = dt.date.today() - dt.timedelta(days=365))
+
+
+    session.close()
+
+ # Convert list of tuples into normal list
+    most_active = list(np.ravel(results))
+
+    return jsonify(most_active)
 
 
 
 
 
+########## MIN, MAX, AVG TOBS  ########################
+# @app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/justice-league/real_name/<start>")
+def tobs_search_start_date(start):
+    """Fetch the date that matches the date supplied by user or a 404 if not."""
+
+    canonicalized = start.replace(" ", "").lower()
+    for date in tobs_search_start_date:
+        search_term = date["start"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return jsonify(date)
+
+    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
 
 
+
+############## Run in terminal  ######################################
 
 if __name__ == '__main__':
     app.run(debug=True)
