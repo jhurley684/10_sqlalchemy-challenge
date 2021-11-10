053e3ae3bec36286635a95afb146a1c86de3c3f4
Jim Hurley <jhurley684@gmail.com>
2021-11-10 09:59:40 -0600

    Last three routes not done

--- Instructions/app.py
+++ Instructions/app.py
@@ -27,6 +27,8 @@ Station = Base.classes.station
 #################################################
 app = Flask(__name__)
 
+# Create our session (link) from Python to the DB
+session = Session(engine)
 
 #################################################
 # Flask Routes
@@ -40,16 +42,16 @@ def welcome():
         f"/api/v1.0/precip<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/active_station<br/>"
+        f"Enter start date as 01-01-21"
+        f"/api/v1.0/start<br/>"
         
     )
-
-
 
 ###############   PRECIPITATION  ##########################
 @app.route("/api/v1.0/precip")
 def precip():
     # Create our session (link) from Python to the DB
-    session = Session(engine)
+    # session = Session(engine)
    
     results = session.query(Measurement.date, Measurement.prcp).all()
 
@@ -63,15 +65,15 @@ def precip():
         precip_dict["precip"] = prcp
         all_precip.append(precip_dict)
 
-    return jsonify(all_precip)
+    return jsonify(precipitation = all_precip)
 
 
 
-#############  STATIONS #########################
+#############  LIST OF STATIONS STATIONS #########################
 @app.route("/api/v1.0/stations")
 def station():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
+    # # Create our session (link) from Python to the DB
+    # session = Session(engine)
 
     """Return a list of stations  from data set"""
     # Query all station measurements in Staton measurements
@@ -85,53 +87,90 @@ def station():
     return jsonify(all_station)
 
 
-# ##########  TOBS BY MOST ACTIVE STATION  ##########################
+
+# ########## MOST ACTIVE STATION   ######################
 
 @app.route("/api/v1.0/active_station")
 def active_station():
-    # Create our session (link) from Python to the DB
+    # # Create session (link) from Python to the DB
     session = Session(engine)
 
     # Query for station activity.  Take the most active station.
-    results = session.query(Measurement.station, func.count(Measurement.prcp)).\
+    results = session.query(Measurement.station,func.count(Measurement.tobs)).\
         group_by(Measurement.station).\
-        order_by(func.count(Measurement.prcp).desc()).first()
-
-# # Query for all tobs measurements at each station. Filter for only the most active station (above)
-    session.query(Measurement.station, Measurement.data, Measurement.tobs ).\
-        filter( Measurement.station == results).\
-        filter(year_ago = dt.date.today() - dt.timedelta(days=365))
-
+        order_by(func.count(Measurement.tobs).desc()).first()
 
     session.close()
 
- # Convert list of tuples into normal list
-    most_active = list(np.ravel(results))
+     # Convert list of tuples into normal list
+    station1 = list(np.ravel(results))
 
-    return jsonify(most_active)
-
-
+    return jsonify(station1)
 
 
+# ########## TOBS AT THE MOST ACTIVE STATION   ######################
+# # # # Query for all tobs measurements at each station. Filter for only the most active station (above)
+@app.route("/api/v1.0/tobs_data")
+def tobs_data():
 
-########## MIN, MAX, AVG TOBS  ########################
-# @app.route("/api/v1.0/<start>")
-@app.route("/api/v1.0/justice-league/real_name/<start>")
-def tobs_search_start_date(start):
-    """Fetch the date that matches the date supplied by user or a 404 if not."""
+    # # Create session (link) from Python to the DB
+    session = Session(engine)
 
-    canonicalized = start.replace(" ", "").lower()
-    for date in tobs_search_start_date:
-        search_term = date["start"].replace(" ", "").lower()
+    most_active = session.query(Measurement.station, func.count(Measurement.tobs)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.tobs).desc()).first()
 
-        if search_term == canonicalized:
-            return jsonify(date)
+    most_active_station = most_active[0]
+    # print(most_active)
+    tobs_station = session.query( Measurement.tobs).\
+        filter(Measurement.station == most_active_station).all()
+       
 
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+    session.close()
+
+     # Convert list of tuples into normal list
+    tobs_station = list(np.ravel(tobs_station))
+
+    return jsonify(temperature = tobs_station)
 
 
 
-############## Run in terminal  ######################################
+# ########## TOBS FOR PREVIOUS YEAR   ######################
+# @app.route("/api/v1.0/tobs_data")
+# def tobs_data():
+
+#     # # Create session (link) from Python to the DB
+#     session = Session(engine)
+
+# # Find the most recent date in the data set.
+#     recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+
+# # Starting from the most recent data point in the database. 
+#     past_year = session.query(Measurement.date, Measurement.prcp).filter(func.strftime("%x", Measurement.date) > (recent_date - 1 year))
+    
+ 
+#     session.close()
+
+#      # Convert list of tuples into normal list
+#     past_year = list(np.ravel(past_year))
+
+#     return jsonify(temperature = past_year)
+
+
+
+
+######## MIN, MAX, AVG TOBS - START DATE ONLY  #############
+# for all stock in the month of May
+# Sort the result by stock name
+
+
+########## MIN, MAX, AVG TOBS - START & END DATE ###########
+
+
+
+
+
+############# Run in terminal  ##############################
 
 if __name__ == '__main__':
     app.run(debug=True)
